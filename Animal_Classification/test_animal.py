61be507316f9fa9b043ac6e0dbf19499e625b375
Akash <akashanandraj555@gmail.com>
2025-07-19 20:17:11 +0530

    Add Animal Classification project

---
+++ Animal_Classification/test_animal.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras.preprocessing import image
+import matplotlib.pyplot as plt
+
+# Load the trained model
+model = tf.keras.models.load_model('best_model.h5')
+
+# Define the class labels (order should match the folders during training)
+class_labels = ['Bear', 'Bird', 'Cat', 'Cow', 'Deer', 'Dog', 'Dolphin', 'Elephant',
+                'Giraffe', 'Horse', 'Kangaroo', 'Lion', 'Panda', 'Tiger', 'Zebra']
+
+# Path to your test image
+img_path = 'Unified_mentor\dataset\Dolphin\Dolphin_2_2.jpg'  # <-- change this
+
+# Load and preprocess the image
+img = image.load_img(img_path, target_size=(224, 224))
+img_array = image.img_to_array(img)
+img_array = img_array / 255.0  # Normalize
+img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
+
+# Predict
+predictions = model.predict(img_array)
+predicted_class = class_labels[np.argmax(predictions)]
+
+# Show result
+plt.imshow(img)
+plt.axis('off')
+plt.title(f"Predicted: {predicted_class}")
+plt.show()
